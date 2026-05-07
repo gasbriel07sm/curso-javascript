6c6ee5d8f263b3c7f0cb5843bb025b26d9fc7d3e
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-07 10:13:06 -0300

    Aula 041 - exercício lógica da programação 02

---
+++ aula041/index.js
@@ -0,0 +1,5 @@
+//function ePaisagem(largura, altura) {
+//    return largura >= altura;
+//}
+const ePaisagem = (largura, altura) => largura >= altura;
+console.log("A imagem está no modeo paisagem? " + ePaisagem(100, 50));
