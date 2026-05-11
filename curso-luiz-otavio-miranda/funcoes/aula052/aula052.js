246c80e966fec4ca2ea26f2317ebf57606a47e0f
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-11 15:07:21 -0300

    Aula 052 - Escopo léxico

---
+++ curso-luiz-otavio-miranda/funcoes/aula052/aula052.js
@@ -0,0 +1,13 @@
+// ESCOPO LÉXICO
+const nome = 'Luiz';
+
+function falaNome() {
+    console.log(nome);
+
+}
+
+function usaFalaNome() {
+    const nome = 'Otávio';
+    falaNome();
+}
+usaFalaNome();
