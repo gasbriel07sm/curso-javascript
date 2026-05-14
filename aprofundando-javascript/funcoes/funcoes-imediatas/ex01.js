2eb294bb5db0a497e0cd1d6126223c314dedc644
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-14 00:11:26 -0300

    javascript - aprofundamento em funções > Funções Imediatas (IIFE)

---
+++ aprofundando-javascript/funcoes/funcoes-imediatas/ex01.js
@@ -0,0 +1,7 @@
+/** Crie uma IIFE que exiba no console:
+    Executando imediatamente 
+*/
+
+(function() {
+    console.log('Executando imediatamente.')
+})();
---
+++ aprofundando-javascript/funcoes/funcoes-imediatas/ex02.js
@@ -0,0 +1,7 @@
+/** Crie uma IIFE que receba um nome como parâmetro e exiba:
+    Bem-vindo, Gabriel 
+*/
+
+(function(nome) {
+    console.log(`Bem-vindo, ${nome}`);
+})('Gabriel');
