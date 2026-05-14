7b5432d8c2dc2f0f745637f0ca7d7bf2ea0f3593
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-14 01:02:23 -0300

    javascript - aprofundamento em arrays > Concatenando arrays

---
+++ aprofundando-javascript/arrays/concatenando/ex01.js
@@ -0,0 +1,5 @@
+const frontend = ['HTML', 'CSS', 'JavaScript'];
+const backend = ['Node.js', 'Express', 'NestJS'];
+
+const juncao = frontend.concat(backend);
+console.log(juncao);
---
+++ aprofundando-javascript/arrays/concatenando/ex02.js
@@ -0,0 +1,5 @@
+const frontend = ['HTML', 'CSS', 'JavaScript'];
+const backend = ['Node.js', 'Express', 'NestJS'];
+
+const juncao = [...frontend, ...backend, 'PostgreSQL'];
+console.log(juncao);
