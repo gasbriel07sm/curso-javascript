fb4e3ff48b328759a9ad7562887bf1a554319bee
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-14 00:56:50 -0300

    javascript - aprofundamento em arrays > Método splice()

---
+++ aprofundando-javascript/arrays/splice/ex02.js
@@ -0,0 +1,6 @@
+// Substitua 'Python' por 'TypeScript' usando splice.
+
+const linguagens = ['HTML', 'CSS', 'Python', 'JavaScript'];
+
+linguagens.splice(2, 1, 'Typescript');
+console.log(linguagens);
